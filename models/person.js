352c62b4d7574dcd776516fa3f3fe88a9c1e949c
Roland Plett <roland@rnplett.org>
2020-02-18 19:04:30 -0700

    Added relations schema to models

--- models/person.js
+++ models/person.js
@@ -3,10 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name: { type: String, required: true },
-  birthdate: { type: Date, required: true },
-  imageUrl: { type: String, required: false },
-  description: { type: String, required: true }
+  name: {
+    type: String,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: false
+  },
+  description: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Person', personSchema);
